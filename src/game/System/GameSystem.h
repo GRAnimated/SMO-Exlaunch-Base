a5b6be17519a7501f4722a4c96dc7eee1922b885
GRAnimated <devin@nhscan.com>
2022-11-29 08:12:31 -0500

    what is going on

--- src/game/System/GameSystem.h
+++ src/game/System/GameSystem.h
@@ -2,11 +2,18 @@
 
 #include "GameSystemInfo.h"
 #include "al/nerve/NerveExecutor.h"
+#include "al/hio/HioNode.h"
 
-class GameSystem : public al::NerveExecutor {
+class GameSystem : public al::NerveExecutor, public al::HioNode {
     public:
+        GameSystem();
+        ~GameSystem();
         void init();
-        void *gap;
+        void setPadName();
+        void tryChangeSequence(char const*);
+        void movement();
+        void drawMain();
+        void exePlay();
         al::GameSystemInfo* mSystemInfo;  // 0x18
         // 0x78 GameConfigData
 };
